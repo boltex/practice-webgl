67245414e0d959944531470d40706fdbd9f1d220
Félix <felixgraphx@gmail.com>
2024-10-03 01:12:59 -0400

    Made progress with "Sprite" object UV cropping.

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,8 @@ export class Game {
 
     public canvasElement: HTMLCanvasElement;
     public gl!: WebGL2RenderingContext;
-    public sprite: Sprite;
+    public sprite1: Sprite;
+    // public sprite2: Sprite;
     public worldSpaceMatrix: M3x3;
 
     private _resizeTimer: ReturnType<typeof setTimeout> | undefined;
@@ -50,7 +51,17 @@ export class Game {
 
         document.body.appendChild(this.canvasElement);
 
-        this.sprite = new Sprite(this.gl, "images/sprite.png", Constants.vertexShaderSource, Constants.fragmentShaderSource);
+        this.sprite1 = new Sprite(
+            this.gl,
+            "images/alien.png",
+            Constants.vertexShaderSource,
+            Constants.fragmentShaderSource,
+            {
+                width: 64,
+                height: 64,
+            }
+        );
+        // this.sprite2 = new Sprite(this.gl, "images/sprite.png", Constants.vertexShaderSource, Constants.fragmentShaderSource);
     }
 
     public update(): void {
@@ -62,7 +73,8 @@ export class Game {
         this.gl.enable(this.gl.BLEND);
         this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
 
-        this.sprite.render();
+        this.sprite1.render();
+        // this.sprite2.render();
 
         this.gl.flush();
 
@@ -78,9 +90,6 @@ export class Game {
 
             const wRatio = w / (h / 240);
             this.worldSpaceMatrix = new M3x3().translation(-1, 1).scale(2 / wRatio, -2 / 240);
-            // this.worldSpaceMatrix = new M3x3().translation(-1.0, 1.0).scale(2 / wRatio, -2 / 240);
-            console.log('RESIZE', w, h);
-            // 
         }, 100);
     }
 
@@ -146,19 +155,28 @@ export class Sprite {
     public aTexCoordLoc!: GLint;
     public uImageLoc!: WebGLUniformLocation;
     public uWorldLoc!: WebGLUniformLocation;
+    public uFrameLoc!: WebGLUniformLocation;
 
-    constructor(gl: WebGL2RenderingContext, imgURL: string, vs: string, fs: string, options: Record<string, any> = {}) {
-
+    constructor(
+        gl: WebGL2RenderingContext,
+        imgURL: string,
+        vs: string,
+        fs: string,
+        options: {
+            height?: number;
+            width?: number;
+        } = {}
+    ) {
         this.gl = gl;
         this.isLoaded = false;
         this.material = new Material(gl, vs, fs);
 
         this.size = new Point(64, 64);
-        if ("width" in options) {
-            this.size.x = options["width"] * 1;
+        if (typeof options.width === 'number') {
+            this.size.x = options.width * 1;
         }
-        if ("height" in options) {
-            this.size.y = options["height"] * 1;
+        if (typeof options.height === 'number') {
+            this.size.y = options.height * 1;
         }
 
         this.image = new Image();
@@ -166,7 +184,6 @@ export class Sprite {
         this.image.onload = () => {
             this.setup();
         }
-
     }
 
     static createRectArray(x = 0, y = 0, w = 1, h = 1) {
@@ -190,16 +207,17 @@ export class Sprite {
         gl.bindTexture(gl.TEXTURE_2D, this.gl_tex);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
-
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.image);
         gl.bindTexture(gl.TEXTURE_2D, null);
 
+        let uv_x = this.size.x / this.image.width;
+        let uv_y = this.size.y / this.image.height;
+
         this.tex_buff = gl.createBuffer()!;
         gl.bindBuffer(gl.ARRAY_BUFFER, this.tex_buff);
-        gl.bufferData(gl.ARRAY_BUFFER, Sprite.createRectArray(), gl.STATIC_DRAW);
+        gl.bufferData(gl.ARRAY_BUFFER, Sprite.createRectArray(0, 0, uv_x, uv_y), gl.STATIC_DRAW);
 
         this.geo_buff = gl.createBuffer()!;
         gl.bindBuffer(gl.ARRAY_BUFFER, this.geo_buff);
@@ -209,6 +227,7 @@ export class Sprite {
         this.aTexCoordLoc = gl.getAttribLocation(this.material.program, "a_texCoord");
         this.uImageLoc = gl.getUniformLocation(this.material.program, "u_texture")!;
 
+        this.uFrameLoc = gl.getUniformLocation(this.material.program, "u_frame")!;
         this.uWorldLoc = gl.getUniformLocation(this.material.program, "u_world")!;
 
         gl.useProgram(null);
@@ -233,6 +252,7 @@ export class Sprite {
             gl.enableVertexAttribArray(this.aPositionLoc);
             gl.vertexAttribPointer(this.aPositionLoc, 2, gl.FLOAT, false, 0, 0);
 
+            // gl.uniform2f(this.uFrameLoc, frame_x, frame_y);
             gl.uniformMatrix3fv(this.uWorldLoc, false, window.game.worldSpaceMatrix.getFloatArray());
 
             gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
